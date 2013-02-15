b45321a4ef23fe79f1a71a4651ec2a5336f0452c
Gav Wood <gav@lancasterlogicresponse.com>
2013-02-15 15:02:10 +0000

    Renamed Fixture to Scene.

--- update.sh
+++ update.sh
@@ -1,16 +1,53 @@
 #!/bin/bash
 
-echo "Building projects..."
-OWD="$PWD"
-for p in Lightbox-Release Lightshow-Release; do
-	cd "../$p"
-	if ! make -j4 2>/tmp/make-out; then
-		cat /tmp/make-out
-		echo "Error building. Stop."
-		exit 1
-	fi
-	cd "$OWD"
+build=0
+projects="Lightbox-Release Lightshow-Release"
+
+function usage
+{
+	echo "Usage: ./update.sh [OPTION]"
+	echo "Update the Pi's software. Copyright Lancaster Logic Response 2013, by Gavin Wood."
+	echo "Options:"
+	echo "  -r        Perform rebuild."
+	echo "  -h        Print this message."
+}
+
+while getopts "rh" opt; do
+	case $opt in
+	r)
+		build=1;;
+	p)
+		projects="$OPTARG";;
+	h)
+		usage
+		exit 0;;
+	\?)
+		echo "Invalid option: -$OPTARG" >&2
+		usage
+		exit 1;;
+	:)
+		echo "Option -$OPTARG requires an argument." >&2
+		usage
+		exit 1;;
+	esac
 done
+
+if [ $build == 1 ]; then
+	echo "Building projects..."
+	OWD="$PWD"
+	for p in $projects; do
+		cd "../$p"
+		make clean 1>/dev/null
+		make qmake 1>/dev/null
+		make -j4 1>/tmp/make-out 2>/tmp/make-err
+		if [ $? != 0 ]; then
+			cat /tmp/make-out /tmp/make-err
+			echo "Error building. Stop."
+			exit 1
+		fi
+		cd "$OWD"
+	done
+fi
 
 ssh root@192.168.69.2 "\
 	echo Mounting read/write...; \
@@ -22,6 +59,8 @@ ssh root@192.168.69.2 "\
 ssh root@192.168.69.2 "cat > /lightbox/Chrome.new" < ../Lightshow-Release/built/DirectChrome
 
 ssh root@192.168.69.2 "\
+	echo Fixing system...; \
+	[ -e /lightbox/fixture ] && mv /lightbox/fixture /lightbox/scene; \
 	echo Starting Chrome...; \
 	mv /lightbox/Chrome.new /lightbox/Chrome; \
 	chmod +x /lightbox/Chrome; \
